398286632a2a0829ff0884a8fef1507aeb03a321
Leeseyoon <custmor@kookmin.ac.kr>
2021-01-21 02:21:06 +0900

    Update lea.h

--- lea.h
+++ lea.h
@@ -3,11 +3,18 @@
 
 void LEA_Enc(unsigned int* P, unsigned int* C, unsigned int* K);
 
-void LEA_Enc_Round(unsigned int* X, unsigned int* X_out, unsigned int* RK);
+void LEA_Enc_Round(unsigned int* X, unsigned int* RK);
 
 int LEA_Enc_KeySch(unsigned int* K, unsigned int* RK);
 
+void LEA_Dec(unsigned int* C, unsigned int* P, unsigned int* K);
+
+void LEA_Dec_Round(unsigned int* X, unsigned int* RK);
+
+int LEA_Dec_KeySch(unsigned int* K, unsigned int* RK);
+
 void ctoi(unsigned char c[], unsigned int* i);
 
+void ctoi_key(unsigned char c[], unsigned int* i);
 
 #endif 
